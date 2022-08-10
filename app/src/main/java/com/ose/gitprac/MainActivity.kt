bb85576b7450011cf2d0f93398c589a32dbbb2db
seyoon5 <82998388+seyoon5@users.noreply.github.com>
2022-08-10 20:20:53 +0900

    Update MainActivity.kt

--- app/src/main/java/com/ose/gitprac/MainActivity.kt
+++ app/src/main/java/com/ose/gitprac/MainActivity.kt
@@ -10,6 +10,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val a = ""
         val b = "3"
+        val c = "5"
+        val d = "7"
         // 새로운 브렌치
         // 추가적인 작업을 통해 수정된 줄.
         // B 에서 실시한 작업.
