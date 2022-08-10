10198839175089fa278d88ed3fdc8b8c00367857
user <tpdbs1208@naver.com>
2022-08-10 17:25:21 +0900

    first commit

---
+++ app/src/main/java/com/ose/gitprac/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.ose.gitprac
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val a = ""
+        // 1
+    }
+}
